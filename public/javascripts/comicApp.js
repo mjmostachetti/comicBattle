e13199720cf711c9a81b6d112ed8c6185f3ee5ef
Mikeronomicon <mikeronomicon@gmail.com>
2015-07-10 14:54:13 -0700

    fixed some issues with loggin in

--- public/javascripts/comicApp.js
+++ public/javascripts/comicApp.js
@@ -37,41 +37,52 @@ $(document).ready(function(){
 
 	var characterList = new CharacterCollection;
 
-	var CharacterView = Backbone.View.extend({
-		tagName: 'li',
-		template: _.template($('#character-info').html()),
-		render : function(){
-			this.$el.html(this.template(this.model))
-		}
-	})
+	// var CharacterView = Backbone.View.extend({
+	// 	tagName: 'li',
+	// 	template: _.template($('#character-info').html()),
+	// 	render : function(){
+	// 		this.$el.html(this.template(this.model))
+	// 	}
+	// })
 
-	var viewArray = [];
+	// var viewArray = [];
 
 	//creating a view for login
 	var LoginView = Backbone.View.extend({
-		el: $(),
-
+		el: '#loginForm',
+		initialize: function(){
+			this.$el.html('<form id="login" action="/login" method="post"><input id="username" name="username" type="text"><input id="password" name="password" type="password"><input id="loginButton" type="submit" value="Login">')
+			console.log(this.$el)
+		},
+		render: function(){
+			console.log("for real, things are working")
+		}
 	})
 
 	var MainAppView = Backbone.View.extend({
-		el: $('#comicapp'),
+		//div in index.jade
+		el: $('#container'),
 		initialize: function(){
+			this.$el.html('<div id="loginForm"></div>')
+			console.log("things are happening")
+			var loginView = new LoginView();
 			// listen to the characterList collection, when a model is added, run this.addCharacter
+			loginView.render();
 			this.listenTo(characterList, 'add', this.addCharacter)
 			characterList.fetch()
 		},
 		addCharacter : function(character){
 			//create new view for this musician
 			console.log(character)
-			var view = new CharacterView({ model : character })
+			//var view = new CharacterView({ model : character })
 			//push the view into array for removal later
-			viewArray.push(view)
-			console.log("This is an array of views : " + view)
-      view.render()
-			this.$("#characters-list").append(view.$el);
+			//viewArray.push(view)
+			// console.log("This is an array of views : " + view)
+   //    view.render()
+			//this.$("#characters-list").append(view.$el);
 		},
 	})
 
-	var App = new MainAppView;
+	var App = new MainAppView();
 
 })
