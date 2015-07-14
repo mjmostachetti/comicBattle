972340ea89004016db4e254e800275b8c95a178a
Justin Gordon <justinwgordon@gmail.com>
2015-07-14 10:16:13 -0700

    working...?

--- public/javascripts/comicApp.js
+++ public/javascripts/comicApp.js
@@ -1,134 +1,129 @@
 $(document).ready(function(){
 
-	//define the character model
+    //define the character model
 
-	var Character = Backbone.Model.extend({
-		defaults : {
-				name : "",
-				energy : 0,
-				strength : 0,
-				magic : 0,
-				deck : ""
-			}
-	})
+    var Character = Backbone.Model.extend({
+        defaults : {
+                name : "",
+                energy : 0,
+                strength : 0,
+                magic : 0,
+                deck : ""
+            }
+    })
 
-	var CharacterCollection = Backbone.Collection.extend({
-		model: Character,
-		url: '/api/characters'
-	})
+    var CharacterCollection = Backbone.Collection.extend({
+        model: Character,
+        url: '/api/characters'
+    })
 
-	var User = Backbone.Model.extend({
-		defaults : {
-				id : 0,
-				username : '',
-				win : 0,
-				loss : 0,
-				hero1 : '',
-				hero2 : '',
-				hero3 : ''
-			}
-	})
+    var User = Backbone.Model.extend({
+        defaults : {
+                id : 0,
+                username : '',
+                win : 0,
+                loss : 0,
+                hero1 : '',
+                hero2 : '',
+                hero3 : ''
+            }
+    })
 
-	var UserCollection = Backbone.Collection.extend({
-		model : User,
-		url: '/users'
-	})
+    var UserCollection = Backbone.Collection.extend({
+        model : User,
+        url: '/users'
+    })
 
-	var characterList = new CharacterCollection;
+    var characterList = new CharacterCollection;
 
-	// var CharacterView = Backbone.View.extend({
-	// 	tagName: 'li',
-	// 	template: _.template($('#character-info').html()),
-	// 	render : function(){
-	// 		this.$el.html(this.template(this.model))
-	// 	}
-	// })
+    // var CharacterView = Backbone.View.extend({
+    //     tagName: 'li',
+    //     template: _.template($('#character-info').html()),
+    //     render : function(){
+    //         this.$el.html(this.template(this.model))
+    //     }
+    // })
 
-	// var viewArray = [];
+    // var viewArray = [];
 
-	//creating a view for login
-	//view creates a div with a tag name to house html
-	//elements in the jade template
-	var LoginView = Backbone.View.extend({
-		tagName : "div",
-		className : "login-view",
-		template : _.template($("#template-login").html()),
-
-		initialize: function(){
-			this.$el.html('<h1> Login / Signup </h1> <h1> Username : </h1> <input id="username" name="username" type="text"> <h1> Password : </h1>  <input id="password" name="password" type="password"> <br><br> <input id="loginButton" type="submit" value="Login"><input id="signupButton" type="submit" value="Signup">')
-			console.log(this.$el)
-			console.log(this.$el);
-			this.render()
-		},
-
-		render: function(){
-			this.$el.html(this.template)
-		}
-	})
-
-	var SignupView = Backbone.View.extend({
-		tagName : "div",
-		className : "signup-view",
-		template : _.template($("#template-signup").html()),
-
-		initialize: function(){
-			this.render()
-		},
-
-		render: function(){
-			this.$el.html(this.template)
-		}
-	})
-	var MainAppView = Backbone.View.extend({
-		//div in index.jade
-
-		el: $('#container'),
-		// el: $('#comicapp'),
-		events : {
-			"click .addChar" : "addCharacterToUserAccount"
-		},
-
-		//el: $('#container'),
-		el: $('#comicapp'),
+    //creating a view for login
+    //view creates a div with a tag name to house html
+    //elements in the jade template
+    var LoginView = Backbone.View.extend({
+        tagName : "div",
+        className : "login-view",
+        template : _.template($("#template-login").html()),
 
 
-		events : {
-			"click .addChar" : "addCharacterToUserAccount"
-		},
-		//main app view initializes loginView, creates a div, and then loads the view.
-		initialize: function(){
-			this.$el.html('<div id="loginForm"></div>')
-			console.log("things are happening")
-			// listen to the characterList collection, when a model is added, run this.addCharacter
-			this.loadLogin();
-			this.listenTo(characterList, 'add', this.addCharacter)
-			characterList.fetch()
-		},
-		//handles loading the login view and html elements
-		loadLogin : function(){
-			var view = new LoginView()
-			this.$el.html(view.$el)
-		},
-		//uses Signup ctor to create SignupView
-		loadSignup : function(){
-			var view = new SignupView()
-			this.$el.html(view.$el)
-		},
-		addCharacter : function(character){
-			//create new view for this musician
-			console.log(character)
-			//var view = new CharacterView({ model : character })
-			//push the view into array for removal later
-			//viewArray.push(view)
-			// console.log("This is an array of views : " + view)
-	 //    view.render()
-			//this.$("#characters-list").append(view.$el);
-		},
-		addCharacterToUserAccount : function(){
-			console.log("run")
-		}
-	})
+        initialize: function(){
+            console.log(this.$el);
+            this.render()
+        },
 
-	var App = new MainAppView();
+        render: function(){
+            this.$el.html(this.template)
+        }
+    })
+
+    var SignupView = Backbone.View.extend({
+        tagName : "div",
+        className : "signup-view",
+        template : _.template($("#template-signup").html()),
+
+        initialize: function(){
+            this.render()
+        },
+
+        render: function(){
+            this.$el.html(this.template)
+        }
+    })
+
+    var MainAppView = Backbone.View.extend({
+        //div in index.jade
+        //el: $('#container'),
+        el: $('#comicapp'),
+
+        events : {
+            "click .addChar" : "addCharacterToUserAccount",
+            "click .signup" : "loadSignup",
+            "click .login" : "loadLogin"
+
+        },
+        //main app view initializes loginView, creates a div, and then loads the view.
+        initialize: function(){
+            this.$el.html('<div id="loginForm"></div>')
+            console.log("things are happening")
+            // listen to the characterList collection, when a model is added, run this.addCharacter
+            this.loadLogin();
+            this.listenTo(characterList, 'add', this.addCharacter)
+            characterList.fetch()
+        },
+        //handles loading the login view and html elements
+        loadLogin : function(){
+            var view = new LoginView()
+            this.$el.html(view.$el)
+        },
+        //uses Signup ctor to create SignupView
+        loadSignup : function(){
+            var view = new SignupView()
+            this.$el.html(view.$el)
+        },
+        addCharacter : function(character){
+            //create new view for this musician
+            console.log(character)
+            //var view = new CharacterView({ model : character })
+            //push the view into array for removal later
+            //viewArray.push(view)
+            // console.log("This is an array of views : " + view)
+     //    view.render()
+            //this.$("#characters-list").append(view.$el);
+        },
+        addCharacterToUserAccount : function(){
+            console.log("run")
+        }
+    })
+
+    var App = new MainAppView();
 
 })
