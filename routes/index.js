53410eca826e0d9fb8f039530317e91657ee2d56
etch11 <colinetch11@gmail.com>
2015-07-24 10:04:44 -0700

    images and html/css files

--- routes/index.js
+++ routes/index.js
@@ -14,6 +14,7 @@ console.log("index.js is running");
 router.get('/teams', function(req, res, next){
 		res.render('select', { title: 'Select Yo Teams' });
 });
+console.log("teams reading");
 
 router.post('/signup', function(request,response){
 	console.log("youve signed up");
