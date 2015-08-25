9b9608296c61bd6fa00c734aba24f748d0fb1474
Mike Mostachetti <mjmostachetti@gmail.com>
2015-08-25 00:48:18 -0700

    Trying to set header...

--- routes/index.js
+++ routes/index.js
@@ -145,8 +145,10 @@ router.get('/api/characters', function(request, response) {
 				res.on('end', function() {
 					console.log("This is res: ")
 					console.log(res.headers['content-type'])
+					console.log(res.headers)
 					if(res.headers['content-type'] === 'text/html'){
 						res.headers['content-type'] = 'application/json'
+						console.log(res.headers)
 					}
 					var charJSON = JSON.parse(writeToThis);
 					//console.log(charJSON.results)
