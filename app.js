cef8e64605c0b0ebb125ebc6103eb140e1a06329
Mike Mostachetti <mjmostachetti@gmail.com>
2015-08-25 12:31:48 -0700

    Added sass middleware

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var sassMiddleware = require('node-sass-middleware')
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -15,6 +16,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+console.log("this is dirname " + __dirname)
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -22,6 +24,11 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(sassMiddleware({
+  src: __dirname,
+  dest: path.join(__dirname,'public'),
+  debug: true
+}))
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.photos
